12b0a20206567190c1a1755f1c818ca019d0002d
Schooleproject <schooleproject123@gmail.com>
2024-12-19 20:31:28 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,64 +10,64 @@ import 'themes/light_theme.dart';
 import 'views/login_page.dart';
 
 
-// void main() {
-//   runApp(
-//     ChangeNotifierProvider(
-//       create: (_) => AppLanguage(),
-//       child: MyApp(),
-//     ),
-//   );
-// }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     final appLanguage = Provider.of<AppLanguage>(context);
-
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       locale: Locale(appLanguage.appLocal),
-//       supportedLocales: [
-//         Locale('en'), // الإنجليزية
-//         Locale('ar'), // العربية
-//       ],
-//       localizationsDelegates: [
-//         GlobalMaterialLocalizations.delegate,
-//         GlobalWidgetsLocalizations.delegate,
-//         GlobalCupertinoLocalizations.delegate,
-//       ],
-//       home: GetxHome(),
-//     );
-//   }
-// }
-
-
-import 'package:get/get.dart';
-import 'Provider/Notifier//theme_controller.dart'; // تأكد من استخدام المسار الصحيح
-import 'config/routes.dart'; // تأكد من استخدام المسار الصحيح
-
 void main() {
-  Get.put(ThemeController());
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => AppLanguage(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    final ThemeController themeController = Get.find();
+    final appLanguage = Provider.of<AppLanguage>(context);
 
-    return Obx(() {
-      return MaterialApp(
-        title: 'Flutter Demo',
-        themeMode: themeController.isDarkMode.value ? ThemeMode.dark : ThemeMode.light,
-        theme: themeController.currentTheme,
-        initialRoute: baseRoute,
-        navigatorKey: RouterManager.navigatorKey,
-        onGenerateRoute: RouterManager.generateRoute,
-        debugShowCheckedModeBanner: false,
-      );
-    });
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      locale: Locale(appLanguage.appLocal),
+      supportedLocales: [
+        Locale('en'), // الإنجليزية
+        Locale('ar'), // العربية
+      ],
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      home: GetxHome(),
+    );
   }
 }
+
+
+// import 'package:get/get.dart';
+// import 'Provider/Notifier//theme_controller.dart'; // تأكد من استخدام المسار الصحيح
+// import 'config/routes.dart'; // تأكد من استخدام المسار الصحيح
+
+// void main() {
+//   Get.put(ThemeController());
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final ThemeController themeController = Get.find();
+
+//     return Obx(() {
+//       return MaterialApp(
+//         title: 'Flutter Demo',
+//         themeMode: themeController.isDarkMode.value ? ThemeMode.dark : ThemeMode.light,
+//         theme: themeController.currentTheme,
+//         initialRoute: baseRoute,
+//         navigatorKey: RouterManager.navigatorKey,
+//         onGenerateRoute: RouterManager.generateRoute,
+//         debugShowCheckedModeBanner: false,
+//       );
+//     });
+//   }
+// }
